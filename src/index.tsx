2cae6696704cc15c884b65aa870543d0a95844d5
IAMISTP <hyeminpak@gmail.com>
2023-10-10 21:11:15 +0900

    Feat: router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import VideoList from "./pages/VideoList";
+import VideoDetail from "./pages/VideoDetail";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/video">
+          <Route path=":searchText" element={<VideoList />} />
+          <Route path="watch/:id" element={<VideoDetail />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
