d0313708e1ae35652a4a6a9f7a5ea8543e640b4c
IAMISTP <hyeminpak@gmail.com>
2023-10-11 20:41:51 +0900

    Feat: 메인페이지 기능구현완료

---
+++ src/pages/Home.tsx
@@ -0,0 +1,89 @@
+import { useQuery } from "@tanstack/react-query";
+import { Videos } from "../apis/data";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import ListVideos from "../components/ ListVideos";
+
+export type ThumbnailsType = {
+  default: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  high: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  medium: {
+    url: string;
+    width: number;
+    height: number;
+  };
+};
+
+export type YoutubeResultType = {
+  etag: string;
+  id: { kind: string; videoId: string };
+  kind: string;
+  snippet: {
+    channelId: string;
+    channelTitle: string;
+    description: string;
+    publishedAt: string;
+    publishTime: string;
+    thumbnails: ThumbnailsType;
+    title: string;
+  };
+};
+
+const Home = () => {
+  const { isLoading, isError, data } = useQuery<YoutubeResultType[], Error>({
+    queryKey: ["videos"],
+    queryFn: Videos,
+    staleTime: 3 * 60 * 1000,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error</span>;
+  }
+
+  return (
+    <Container>
+      <ItemList>
+        {data?.map((video: YoutubeResultType) => {
+          return <ListVideos key={video.etag} video={video} />;
+        })}
+      </ItemList>
+    </Container>
+  );
+};
+
+export default Home;
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+const ItemList = styled.div`
+  display: grid;
+  @media (min-width: 1600px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  @media (min-width: 1024px) and (max-width: 1599px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (min-width: 768px) and (max-width: 1023px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 480px) and (max-width: 767px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
